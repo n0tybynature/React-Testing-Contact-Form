48e739e804fd2510e1f0f55c16135e3d0051858a
n0tybynature <74768895+n0tybynature@users.noreply.github.com>
2021-02-11 17:45:59 -0800

    added expect and userEvent

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
 import App from "../App";
+import userEvent from "@testing-library/user-event"
 
 test("testing testing", () => {
     console.log("hello")
@@ -15,9 +16,24 @@ describe("test two", () => {
 
 
 test("Header Working!", () => {
-   const { debug } = render(<App/>);
-   debug();
+   render(<App/>);
+   
 
-   const firstName = screen.queryByLabelText(/first name/i);
-   const email = screen.queryByLabelText(/email/i);
+   const firstName = screen.getByLabelText(/first name/i);
+   const lastName = screen.getByLabelText(/last name/i);
+   const email = screen.getByLabelText(/email/i);
+   const message = screen.getByLabelText(/message/i);
+
+
+
+    userEvent.type(firstName, "Ton");
+    userEvent.type(lastName,"Yang");
+    userEvent.type(email, "tony.yang5@yahoo.com");
+    userEvent.type(message, "well done tony");
+
+
+    expect(firstName).toHaveValue("Ton");
+    expect(lastName).toHaveValue("Yang");
+    expect(email).toHaveValue("tony.yang5@yahoo.com");
+    expect(message).toHaveValue("well done tony");
 })
